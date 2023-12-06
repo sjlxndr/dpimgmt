4d20b34a6b432caff650427f7b960ac3bad45cd8
sjlxndr <1474962+sjlxndr@users.noreply.github.com>
2023-12-06 14:16:47 -0600

    Update dpitoggle.sh

--- dpitoggle.sh
+++ dpitoggle.sh
@@ -5,10 +5,12 @@
 # bind this to a convenient key combination, like <Super><0>
 
 current=`xfconf-query -c xsettings -p /Xft/DPI`
+default="95" # 95 is default on my system; default also corresponds to "-1"
+target=106
 
-if [ $current -ne "95" ]
+if [ $current -ne $default ]
 then
-    xfconf-query -c xsettings -p /Xft/DPI -s 95
+    xfconf-query -c xsettings -p /Xft/DPI -s $default
 else
-    xfconf-query -c xsettings -p /Xft/DPI -s 106
+    xfconf-query -c xsettings -p /Xft/DPI -s $target
 fi
